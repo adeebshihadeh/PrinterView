69fc5f90c3f89bd27ec2fbbb6fd36a71fc9b6b9c
quillford <adeebshihadeh@gmail.com>
2015-03-16 21:10:48 -0700

    Printer name

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 var printerIP = "pb.local";
 var apiKey = "25A1AE457F3E4ACF854B80A51BA51776";
 
+function initialInfo(){
+    $.ajaxSetup({
+        headers : {
+            "X-Api-Key" : apiKey
+        }
+    });
+    $.getJSON("http://"+printerIP+"/api/printerprofiles", function(json){document.getElementById("title").innerHTML="Printer: "+json.profiles._default.name});
+
+    updatePrinterStatus();
+}
+
 function updatePrinterStatus(){
 
     $.ajaxSetup({
@@ -12,4 +23,4 @@ function updatePrinterStatus(){
 
 }
 
-updatePrinterStatus();
+initialInfo();
