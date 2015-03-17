f6d4b969ac963c32b7f5d69c0f1b0e0669ca6401
quillford <adeebshihadeh@gmail.com>
2015-03-17 15:58:31 -0700

    Printer ip

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 var printerIP = "prusa.local";
 var apiKey = "156A8AE4000940CFB3C51C9DFD812D8A";
 
+window.onload = function(){
+    initialInfo();
+}
+
 function initialInfo(){
     // add apikey header to GET request
     $.ajaxSetup({headers:{"X-Api-Key" : apiKey}});
     // get name of the printer
     $.getJSON("http://"+printerIP+"/api/printerprofiles", function(json){document.getElementById("printerName").innerHTML=json.profiles._default.name});
+    document.getElementById("printerIP").innerHTML = printerIP;
 
     updatePrinterStatus();
 }
@@ -20,10 +25,10 @@ function updatePrinterStatus(){
         document.getElementById("printerStatus").innerHTML="State: "+json.state;
         //get filename of print
         document.getElementById("currentFile").innerHTML="File: "+json.job.file.name.split(".").slice(0, -1).join(".");
-        // get percentage of print completion
-        document.getElementById("printPercent").innerHTML=json.progress.completion.toFixed(2)*100+"%";
         // get estimation of print time left
         document.getElementById("timeLeft").innerHTML="Time left: "+json.progress.printTimeLeft/60 + " minutes";
+        // get percentage of print completion
+        document.getElementById("printPercent").innerHTML=json.progress.completion.toFixed(2)*100+"%";
         });
 
     // get info on temps
@@ -34,5 +39,3 @@ function updatePrinterStatus(){
         document.getElementById("bedTemp").innerHTML="Bed: "+json.temperature.bed.actual+"°/"+json.temperature.bed.target+"°";
     });
 }
-
-initialInfo();
