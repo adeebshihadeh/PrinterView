2bfde04f4e9c212b223c7e8562324c65dfad99f3
quillford <adeebshihadeh@gmail.com>
2015-03-19 06:58:32 -0700

    Fixed header UI

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ var printers ={
 window.onload = function(){
 
     //initialInfo();
-//     addPrinter("prusa.local", "156A8AE4000940CFB3C51C9DFD812D8A");
-//     addPrinter("pb.local", "25A1AE457F3E4ACF854B80A51BA51776");
-    addPrinter(" ", "156A8AE4000940CFB3C51C9DFD812D8A");
-    addPrinter(" ", "25A1AE457F3E4ACF854B80A51BA51776");
+    addPrinter("prusa.local", "156A8AE4000940CFB3C51C9DFD812D8A");
+    addPrinter("pb.local", "25A1AE457F3E4ACF854B80A51BA51776");
+    // addPrinter(" ", "156A8AE4000940CFB3C51C9DFD812D8A");
+    // addPrinter(" ", "25A1AE457F3E4ACF854B80A51BA51776");
     setInterval(function () {updatePrinters();}, refreshRate);
 }
 
@@ -61,25 +61,21 @@ function addPrinter(ip, apikey){
   var printerNum = numPrinters;
 
   // add HTML
-  $("#printerPanels").append('<div class="panel panel-primary">');
-  $("#printerPanels").append('<div class="panel-heading" id="printerName'+printerNum+'">Printer 1</div>');
-  $("#printerPanels").append('<div class="panel-body" id="body'+printerNum+'">');
-  
-  $("#body"+printerNum).append('<div id="printerInfo'+printerNum+'">');
+  $("#printerPanels").append('<div class="panel panel-primary" id="panel'+printerNum+'"></div>');
+  $("#panel"+printerNum).append('<div class="panel-heading" id="printerName'+printerNum+'">Printer Name</div>');
+  $("#panel"+printerNum).append('<div class="panel-body" id="body'+printerNum+'"></div>');
+
   $("#body"+printerNum).append('<p id="printerStatus'+printerNum+'">status</p>');
   $("#body"+printerNum).append('<p id="e0Temp'+printerNum+'">0</p>');
   $("#body"+printerNum).append('<p id="bedTemp'+printerNum+'">0</p>');
   $("#body"+printerNum).append('<p id="currentFile'+printerNum+'">No active print</p>');
   $("#body"+printerNum).append('<p id="timeLeft'+printerNum+'">Print Time Left</p>');
-  
+
   $("#body"+printerNum).append('<div class="progress" id="progress'+printerNum+'"></div>');
   $("#progress"+printerNum).append('<div class="progress-bar progress-bar-info progress-bar-striped active" role="progressbar" aria-valuenow="15" aria-valuemin="0" aria-valuemax="100" style="width: 20%"  id="progressBar'+printerNum+'"></div>');
   $("#progressBar"+printerNum).append('<span class="sr-only" id="progressPercent'+printerNum+'">15% Complete</span>');
-  
-  $("#printerPanels").append('</div>');
-  $("#printerPanels").append('</div>');
+
   $("#printerPanels").append('<div class="panel-footer" id="printerIP'+printerNum+'">ip</div>');
-  $("#printerPanels").append('</div>');
 
   // store ip and apikey info
   printers.ip[printerNum]=ip;
