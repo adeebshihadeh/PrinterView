ec28c94407416b46924c9d42f2826659eb8cf316
quillford <adeebshihadeh@gmail.com>
2015-03-19 21:02:11 -0700

    Printers are now saved

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 var refreshRate = 10000; // in milliseconds
 var numPrinters = 0;
-var printers ={
-    "ip":[],
-    "apikey":[]
-};
+var printers = new Object;
 
 // TODO
 // verify octoprint ip and apikey
@@ -12,11 +9,26 @@ var printers ={
 // save/cache printer IPs and API keys
 
 window.onload = function(){
+    // get saved printers
+    reloadPrinters();
 
-    //initialInfo();
-    addPrinter("prusa.local", "156A8AE4000940CFB3C51C9DFD812D8A");
-    //addPrinter("pb.local", "25A1AE457F3E4ACF854B80A51BA51776");
+    // update printer info
     setInterval(function () {updatePrinters();}, refreshRate);
+}
+
+function reloadPrinters(){
+    if(localStorage.getItem("savedPrinters") == null){
+        printers ={
+            "ip":[],
+            "apikey":[]
+        };
+        $("#noPrintersModal").modal("show");
+    }else {
+        printers = JSON.parse(localStorage.getItem("savedPrinters"));
+        for(var i=0;i<printers.ip.length;i++){
+            addPrinter(printers.ip[i], printers.apikey[i]);
+        }
+    }
 }
 
 function initialInfo(ip, apikey, index){
@@ -44,7 +56,7 @@ function updateStatus(ip, apikey, index){
         // get percentage of print completion
         document.getELementById("#progress"+index).attr("aria-valuenow", json.progress.completion.toFixed(0)*1);
         document.getElementById("#progressBar"+index).innerHTML=json.progress.completion.toFixed(0)*1+"%";
-        });
+    });
 
     // get info on temps
     $.getJSON("http://"+ip+"/api/printer", function(json){
@@ -56,50 +68,54 @@ function updateStatus(ip, apikey, index){
 }
 
 function updatePrinters(){
-  for(var i=0;i<numPrinters;i++){
-    updateStatus(printers.ip[i],printers.apikey[i], i);
-  }
+    for(var i=0;i<numPrinters;i++){
+        updateStatus(printers.ip[i],printers.apikey[i], i);
+    }
 }
 
 function addPrinter(ip, apikey){
-  var printerNum = numPrinters;
+    //var numPrinters = printers.ip.length;
+    var printerNum = numPrinters;
 
-  // add HTML
-  $("#printerPanels").append('<div class="panel panel-primary" id="panel'+printerNum+'"></div>');
-  $("#panel"+printerNum).append('<div class="panel-heading" id="printerName'+printerNum+'">Printer Name</div>');
-  $("#panel"+printerNum).append('<div class="panel-body" id="body'+printerNum+'"></div>');
+    // add HTML
+    $("#printerPanels").append('<div class="panel panel-primary" id="panel'+printerNum+'"></div>');
+    $("#panel"+printerNum).append('<div class="panel-heading" id="printerName'+printerNum+'">Printer Name</div>');
+    $("#panel"+printerNum).append('<div class="panel-body" id="body'+printerNum+'"></div>');
 
-  $("#body"+printerNum).append('<p id="printerStatus'+printerNum+'">status</p>');
-  $("#body"+printerNum).append('<p id="e0Temp'+printerNum+'">0</p>');
-  $("#body"+printerNum).append('<p id="bedTemp'+printerNum+'">0</p>');
-  $("#body"+printerNum).append('<p id="currentFile'+printerNum+'">No active print</p>');
-  $("#body"+printerNum).append('<p id="timeLeft'+printerNum+'">Print Time Left</p>');
+    $("#body"+printerNum).append('<p id="printerStatus'+printerNum+'">status</p>');
+    $("#body"+printerNum).append('<p id="e0Temp'+printerNum+'">0</p>');
+    $("#body"+printerNum).append('<p id="bedTemp'+printerNum+'">0</p>');
+    $("#body"+printerNum).append('<p id="currentFile'+printerNum+'">No active print</p>');
+    $("#body"+printerNum).append('<p id="timeLeft'+printerNum+'">Print Time Left</p>');
 
-  $("#body"+printerNum).append('<div class="progress" id="progress'+printerNum+'"></div>');
-  $("#progress"+printerNum).append('<div class="progress-bar progress-bar-info progress-bar-striped active" role="progressbar" aria-valuenow="15" aria-valuemin="0" aria-valuemax="100" style="width: 20%"  id="progressBar'+printerNum+'"></div>');
-  $("#progressBar"+printerNum).append('<span class="sr-only" id="progressPercent'+printerNum+'">15% Complete</span>');
+    $("#body"+printerNum).append('<div class="progress" id="progress'+printerNum+'"></div>');
+    $("#progress"+printerNum).append('<div class="progress-bar progress-bar-info progress-bar-striped active" role="progressbar" aria-valuenow="15" aria-valuemin="0" aria-valuemax="100" style="width: 20%"  id="progressBar'+printerNum+'"></div>');
+    $("#progressBar"+printerNum).append('<span class="sr-only" id="progressPercent'+printerNum+'">15% Complete</span>');
 
-  $("#panel"+printerNum).append('<div class="panel-footer" id="printerIP'+printerNum+'">ip</div>');
+    $("#panel"+printerNum).append('<div class="panel-footer" id="printerIP'+printerNum+'">ip</div>');
 
-  // store ip and apikey info
-  printers.ip[printerNum]=ip;
-  printers.apikey[printerNum]=apikey;
+    // store ip and apikey info
+    printers.ip[printerNum]=ip;
+    printers.apikey[printerNum]=apikey;
 
-  // get initial info on printer
-  initialInfo(ip, apikey, printerNum);
+    // save new printer to local storage
+    localStorage.setItem("savedPrinters", JSON.stringify(printers));
 
-  numPrinters++;
+    // get initial info on printer
+    initialInfo(ip, apikey, printerNum);
+
+    numPrinters++;
 }
 
 function addFromModal(){
-  var newIP = $("#newIP").val();
-  var newApikey = $("#newApikey").val();
+    var newIP = $("#newIP").val();
+    var newApikey = $("#newApikey").val();
 
-  if(newIP == ""|| newApikey == ""){
-  	$("#missingInfoModal").modal("show");
-  }else {
-  	addPrinter(newIP, newApikey);
-  	$("#newIP").val("");
-  	$("#newApikey").val("");
-  }
+    if(newIP == ""|| newApikey == ""){
+        $("#missingInfoModal").modal("show");
+    }else {
+        addPrinter(newIP, newApikey);
+        $("#newIP").val("");
+        $("#newApikey").val("");
+    }
 }
